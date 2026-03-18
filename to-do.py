a82c2dad70df3821a7424e70da64b342ed79ee72
vivi 3 <vitoriagcavalcantesales@gmail.com>
2026-03-18 14:17:26 -0300

    primeiro código

---
+++ to-do.py
@@ -0,0 +1,16 @@
+# app.py
+tarefas = []
+
+def adicionar_tarefa(tarefa):
+    tarefas.append(tarefa)
+    print(f"Tarefa  adicionada!")
+
+def listar_tarefas():
+    print("\n--- Lista de Tarefas ---")
+    for i, tarefa in enumerate(tarefas, 1):
+        print(f"{i}.")
+
+# Uso da aplicação
+adicionar_tarefa("Estudar Python")
+adicionar_tarefa("Criar código simples")
+listar_tarefas()
